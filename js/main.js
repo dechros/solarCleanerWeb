7cfa611ebf686a142fc9e3e0a5915b2570a233e8
Halit Cetin <halitcetin@live.com>
2023-06-07 15:40:31 +0200

    visual update again

--- js/main.js
+++ js/main.js
@@ -29,4 +29,23 @@ document.addEventListener('DOMContentLoaded', function () {
     closeIcon.addEventListener('click', closeSideMenu);
     signOutLink.addEventListener('click', signOut);
     overlay.addEventListener('click', closeOnOverlayClick);
+
+    const menuItems = document.querySelectorAll('.side-menu li');
+    const pageIcon = document.querySelector('.page-icon');
+
+    menuItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            const iconClass = this.querySelector('i').className;
+            pageIcon.className = `fas ${iconClass} page-icon`;
+        });
+    });
+
+    const pageHeaderText = document.querySelector('.page-header-text span');
+
+    menuItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            const menuItemText = this.textContent.trim();
+            pageHeaderText.textContent = menuItemText;
+        });
+    });
 });
